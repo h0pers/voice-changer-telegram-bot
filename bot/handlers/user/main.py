b95e735da44daa6cd835a0d81e6fb98abf1608b9
Dmytro Hryshchenko <dmytrohryshchenkowork@gmail.com>
2024-08-13 12:53:08 +0100

    Small FIX. Back Voice Handler is first in routers

--- bot/handlers/user/main.py
+++ bot/handlers/user/main.py
@@ -8,7 +8,7 @@ user_router = Router()
 
 
 def get_user_router() -> Router:
-    user_routers = (start_router, voice_router, voice_return_router,
+    user_routers = (start_router, voice_return_router, voice_router,
                     get_user_callback_router(),)
     user_router.include_routers(*user_routers)
 
